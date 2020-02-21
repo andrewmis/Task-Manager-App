0bae9d8219bea09b9e9adade280a124c820c31d3
Andrew Mis <Andrew.Mis@Hyland.com>
2020-02-20 20:26:06 -0500

    Updating documentation on services

--- todo-list-app/src/app/services/list-manager.service.ts
+++ todo-list-app/src/app/services/list-manager.service.ts
@@ -3,7 +3,17 @@ import { TaskList } from '../models/task-list.model';
 import { ListApiService } from './list-api.service';
 import { v4 as GenerateUuid } from 'uuid';
 import { GetTodaysDate } from '../shared/utilities';
-import { TaskManagerService } from './task-manager.service';
+
+// The default template to use when creating a list.
+const quickAddTemplate: TaskList = {
+  id: '',
+  title: '',
+  description: '',
+  allTasksCompleted: false,
+  allTasksCompletedDate: '',
+  allTasksDueDate: '',
+  tasks: []
+};
 
 @Injectable({
   providedIn: 'root'
@@ -11,17 +21,6 @@ import { TaskManagerService } from './task-manager.service';
 export class ListManagerService {
   public taskLists: TaskList[] = [];
 
-  private quickAddTemplate: TaskList = {
-    id: '',
-    title: '',
-    description: '',
-    allTasksCompleted: false,
-    allTasksCompletedDate: '',
-    allTasksDueDate: '',
-    tasks: []
-  };
-
-  // tslint:disable-next-line: variable-name
   private _isCreatingList: boolean;
   private _isEditingList: boolean;
   private _isViewingList: boolean;
@@ -37,101 +36,164 @@ export class ListManagerService {
     });
   }
 
+  /**
+   * If a list is currently being edited.
+   */
   public get isEditingList(): boolean {
     return this._isEditingList;
   }
 
+  /**
+   * If a list is currently being created.
+   */
   public get isCreatingList(): boolean {
     return this._isCreatingList;
   }
 
+  /**
+   * If a list is currently being viewed.
+   */
   public get isViewingList(): boolean {
     return this._isViewingList;
   }
 
+  /**
+   * The list currently being edited.
+   */
   public get listUnderEdit(): TaskList {
     return this._listUnderEdit;
   }
 
+  /**
+   * The list currently being viewed.
+   */
   public get listBeingViewed(): TaskList {
     return this._listBeingViewed;
   }
 
-  public deleteList(listData: TaskList): void {
-    const filteredLists = this.taskLists.filter(list => list.id !== listData.id);
-
-    if (this._isViewingList) {
-      if (this.listBeingViewed.id === listData.id) {
-        this.resetStates();
-      }
-    }
-
+  /**
+   * Deletes a list with the given id.
+   * @param listId The id of the list to remove.
+   */
+  public deleteList(listId: string): void {
+    // Get array of lists without given list
+    const filteredLists = this.taskLists.filter(list => list.id !== listId);
+    // Apply that filtered list to the local service store
     this.taskLists = filteredLists;
   }
 
+  /**
+   * Creates a new list with the given data and appends it to the list store.
+   * @param listData The data of the list to create.
+   */
   public createList(listData: TaskList): void {
+    // Start with a list default
     const newList: any = {};
-    Object.assign(newList, this.quickAddTemplate);
+    Object.assign(newList, quickAddTemplate);
+
+    // Apply new ID and given properties
     newList.id = GenerateUuid();
     newList.title = listData.title;
     newList.description = listData.description;
     newList.allTasksDueDate = listData.allTasksDueDate;
 
+    // Push to store and end creation state
     this.taskLists.push(newList);
     this.endCreateList();
   }
 
+  /**
+   * Creates a new list with just the given name set.
+   * @param name The name of the list to create.
+   */
   public quickCreateList(name: string) {
+    // Start with a list default
     const newList: any = {};
-    Object.assign(newList, this.quickAddTemplate);
+    Object.assign(newList, quickAddTemplate);
 
     // Populate with name and new id
     newList.id = GenerateUuid();
     newList.title = name;
     newList.dateCreated = GetTodaysDate();
 
+    // Push to store
     this.taskLists.push(newList);
   }
 
+  /**
+   * Exits the list creation state.
+   */
   public cancelCreateState() {
     this._isCreatingList = false;
   }
 
+  /**
+   * Returns the given list's completed task count.
+   */
   public getCompletedTaskCount(list: TaskList): number {
     return list.tasks.filter(task => task.isComplete).length;
   }
 
+  /**
+   * Returns the given list's task count.
+   */
   public getTaskCount(list: TaskList): number {
     return list.tasks.length;
   }
 
+  /**
+   * Checks if a given list name is already in use by a list.
+   * @param name The name to check.
+   */
   public doesListExistWithName(name: string): boolean {
     return !!this.taskLists.find(list => list.title === name);
   }
 
+  /**
+   * Checks if a given task name is already in use by a task.
+   * @param name The task name to check.
+   */
   public doesTaskNameExistOnListBeingViewed(name: string): boolean {
     return !!this._listBeingViewed.tasks.find(task => task.title === name);
   }
 
+  /**
+   * Enter the list creation state.
+   */
   public beginCreateList(): void {
     this._isCreatingList = true;
   }
 
+  /**
+   * Exit the list creation state.
+   */
   public endCreateList(): void {
     this._isCreatingList = false;
   }
 
+  /**
+   * Enter the list viewing state.
+   * @param list The list to begin viewing.
+   */
   public beginViewingList(list: TaskList): void {
     this._isViewingList = true;
     this._listBeingViewed = list;
   }
 
+  /**
+   * Enter the list editing state.
+   * @param list The list to begin editing.
+   */
   public beginEditingList(list: TaskList): void {
     this.stopViewingList();
     this._isEditingList = true;
     this._listUnderEdit = list;
   }
 
+  /**
+   * Applies a set of edits to the list being edited and exits the edit state.
+   * @param edits The edits to save to the list under edit.
+   */
   public saveEditsToList(edits: TaskList): void {
     // Save edits
     this.listUnderEdit.title = edits.title;
@@ -142,21 +204,26 @@ export class ListManagerService {
     this.stopEditingList();
   }
 
+  /**
+   * Exits the list editing state and returns to viewing state.
+   */
   public cancelEditingList(): void {
     this.stopEditingList();
   }
 
-  private stopEditingList(): void {
-    this.beginViewingList(this.listUnderEdit);
-    this._listUnderEdit = undefined;
-    this._isEditingList = false;
-  }
-
+  /**
+   * Exits the list viewing state.
+   */
   public stopViewingList(): void {
     this._isViewingList = false;
     this._listBeingViewed = undefined;
   }
 
+  /**
+   * Toggle/Untoggles all of the tasks in the list with the given id.
+   * @param listId The id of the list to toggle the tasks of.
+   * @param newCompleteValue The new task completion value.
+   */
   public toggleAllTasks(listId, newCompleteValue): void {
     const listToToggle = this.taskLists.find(list => list.id === listId);
 
@@ -175,21 +242,31 @@ export class ListManagerService {
     listToToggle.allTasksCompleted = newCompleteValue;
   }
 
+  /**
+   * Checks if a list is in/complete and updates it's completion property.
+   * @param list (Optional) The list to check.
+   */
   public checkIfListIsComplete(list?: TaskList): void {
     const listToCheck = list ? list : this.listBeingViewed;
     const incompleteTasks = listToCheck.tasks.filter(task => !task.isComplete);
 
     if (incompleteTasks.length < 1) {
+      // Complete list
       listToCheck.allTasksCompleted = true;
       listToCheck.allTasksCompletedDate = GetTodaysDate();
     } else {
+      // Incomplete list
       listToCheck.allTasksCompleted = false;
       listToCheck.allTasksCompletedDate = undefined;
     }
   }
 
-  private resetStates() {
-    this.stopViewingList();
-    this.cancelEditingList();
+  /**
+   * Exits the list editing state and returns to viewing state.
+   */
+  private stopEditingList(): void {
+    this.beginViewingList(this.listUnderEdit);
+    this._listUnderEdit = undefined;
+    this._isEditingList = false;
   }
 }
--- todo-list-app/src/app/services/task-manager.service.ts
+++ todo-list-app/src/app/services/task-manager.service.ts
@@ -3,61 +3,79 @@ import { Task } from '../models/task.model';
 import { ListManagerService } from './list-manager.service';
 import { v4 as GenerateUuid } from 'uuid';
 
+// The default template to use when creating a task.
+const quickAddTemplate: Task = {
+  parentListId: '',
+  id: '',
+  title: '',
+  description: '',
+  priority: null,
+  isComplete: false,
+  dateDue: null,
+  dateCompleted: null
+};
+
 @Injectable({
   providedIn: 'root'
 })
 export class TaskManagerService {
 
-  private quickAddTemplate: Task = {
-    parentListId: '',
-    id: '',
-    title: '',
-    description: '',
-    priority: null,
-    isComplete: false,
-    dateDue: null,
-    dateCompleted: null
-  };
 
-  // tslint:disable-next-line: variable-name
   private _isCreatingTask: boolean;
-  // tslint:disable-next-line: variable-name
   private _isEditingTask: boolean;
-
   private _taskUnderEdit: Task;
 
-  constructor(private _listManager: ListManagerService) {
+  constructor(private _listManager: ListManagerService) {}
 
-  }
-
-  public get taskUnderEdit(): Task {
-    return this._taskUnderEdit;
-  }
-
+  /**
+   * If a task is currently being created.
+   */
   public get isCreatingTask(): boolean {
     return this._isCreatingTask;
   }
 
+  /**
+   * If a task is currently being edited.
+   */
   public get isEditingTask(): boolean {
     return this._isEditingTask;
   }
 
+  /**
+   * The task currently being edited.
+   */
+  public get taskUnderEdit(): Task {
+    return this._taskUnderEdit;
+  }
+
+  /**
+   * Creates a new task with just the given name set.
+   * @param name The name of the task to create.
+   */
   public quickCreateTask(name: string) {
+    // Start with a task default
     const newTask: any = {};
-    Object.assign(newTask, this.quickAddTemplate);
+    Object.assign(newTask, quickAddTemplate);
 
     // Populate with name and new id
     newTask.parentListId = this._listManager.listBeingViewed.id;
     newTask.id = GenerateUuid();
     newTask.title = name;
 
+    // Push to store
     this.pushTaskToList(newTask);
   }
 
+  /**
+   * Creates a new task with the given data and appends it to the task store.
+   * @param taskData The data of the task to create.
+   */
   public createTask(taskData: Task): void {
+        // Start with a task default
     const newTask: any = {};
-    Object.assign(newTask, this.quickAddTemplate);
+    Object.assign(newTask, quickAddTemplate);
 
+    // Apply parent ID, new ID, and given properties
     newTask.parentListId = this._listManager.listBeingViewed.id;
     newTask.id = GenerateUuid();
     newTask.title = taskData.title;
@@ -65,28 +83,47 @@ export class TaskManagerService {
     newTask.priority = taskData.priority;
     newTask.dateDue = taskData.dateDue;
 
+    // Push to store and end creation state
     this.pushTaskToList(newTask);
     this.stopCreatingTask();
   }
 
+  /**
+   * Deletes a task with the given id.
+   * @param taskId The id of the task to remove.
+   */
   public deleteTask(taskId: string): void {
+    // Get the list current being viewed
     const listToRemoveFrom = this._listManager.listBeingViewed;
+    // Remove the given task from that list
     const filteredTasks = listToRemoveFrom.tasks.filter(task => task.id !== taskId);
     listToRemoveFrom.tasks = filteredTasks;
   }
 
+  /**
+   * Enter the task creation state.
+   */
   public beginCreateTask(): void {
+    // Guard in case a task is being edited
     this.cancelEditingTask();
     this._isCreatingTask = true;
   }
 
+  /**
+   * Enter the task editing state.
+   */
   public beginEditingTask(task: Task): void {
+    // Guard in case a task is being created
     this.cancelCreateTask();
 
     this._isEditingTask = true;
     this._taskUnderEdit = task;
   }
 
+  /**
+   * Applies a set of edits to the task being edited and exits the edit state.
+   * @param edits The edits to save to the task under edit.
+   */
   public saveEditsToList(edits: Task): void {
     // Save edits
     this._taskUnderEdit.title = edits.title;
@@ -98,10 +135,16 @@ export class TaskManagerService {
     this.stopEditingTask();
   }
 
+  /**
+   * Exit the task editing state.
+   */
   public cancelEditingTask(): void {
     this.stopEditingTask();
   }
 
+  /**
+   * Exit the task creation state.
+   */
   public cancelCreateTask(): void {
     this.stopCreatingTask();
   }
